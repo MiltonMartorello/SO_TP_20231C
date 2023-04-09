f7eedc8de9d1e36a1731f85ed2d1f375dd613240
Camila Sanchez <casanchez@frba.utn.edu.ar>
2023-04-09 21:14:52 +0000

    memoria.h

---
+++ memoria/src/memoria.h
@@ -0,0 +1,32 @@
+#ifndef MEMORIA_H_
+#define MEMORIA_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <shared.h>
+
+typedef struct {
+	int socket;
+	t_log *log;
+} t_args_hilo_cliente;
+
+
+typedef struct{
+	char* puerto_escucha;
+	uint32_t tam_memoria;
+	uint32_t tam_segmento_0;
+	uint32_t cant_segmentos;
+	uint32_t retardo_memoria;
+	uint32_t retardo_compactacion;
+	char* algoritmo_asignacion;
+}t_memoria_config;
+
+t_memoria_config* leer_config(char *path);
+void correr_servidor(t_log *logger, char *puerto) ;
+int escuchar_clientes(int server_fd, t_log *logger);
+int aceptar_cliente(int socket_servidor);
+void procesar_cliente(void *args_hilo);
+void terminar_programa(int conexion, t_log* logger, t_config* config);
+
+
+#endif
