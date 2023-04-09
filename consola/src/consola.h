fbc22566ca6cc034c5c983165da71da31f8961ef
MiltonMartorello <miltonmartorello@gmail.com>
2023-04-09 13:25:33 +0000

    consola levanta configuración desde archivo

---
+++ consola/src/consola.h
@@ -0,0 +1,12 @@
+#ifndef CONSOLA_H_
+#define CONSOLA_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <shared.h>
+#include <commons/config.h>
+
+#define PATH_CONFIG "consola.config"
+#define PATH_LOG "consola.log"
+
+#endif /* CONSOLA_H_ */
