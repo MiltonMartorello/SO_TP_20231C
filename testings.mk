8d088671e69b18f9f8f44185ad871af2e6df8686
cnsanchez <49007787+cnsanchez@users.noreply.github.com>
2023-07-14 17:35:01 -0300

    Create testings.mk

---
+++ testings.mk
@@ -0,0 +1,20 @@
+ifeq ($(TESTS_ENABLED),1)
+.PHONY: test
+test: all
+	valgrind --tool=none ./$(TEST)
+
+.PHONY: test-daemon
+test-daemon:
+	@test $(shell which entr) || entr
+	while sleep 0.1; do \
+		find tests/ src/ | entr -d make test --no-print-directory; \
+	done
+
+.PHONY: test-memcheck
+test-memcheck: all
+	valgrind --leak-check=full $(MEMCHECK_FLAGS) ./$(TEST)
+
+.PHONY: test-helgrind
+test-helgrind: all
+	valgrind --tool=helgrind $(HELGRIND_FLAGS) ./$(TEST)
+endif
